62423b0a05de358ade494cd9b6e5b86f226d646e
Regis <regis.lopes.eso@gmail.com>
2025-04-10 18:02:38 -0300

    Modify explore page 9 - implementing Find a Board Game

--- js/components/header.js
+++ js/components/header.js
@@ -5,11 +5,42 @@ function loadHeader() {
     <header class="header">
       <a href="#home" class="logo">BBG <span>LIKE</span></a>
       <nav class="navbar">
-        <a href="index.html">HOME</a>
-        <a href="explore.html">EXPLORE</a>
-        <a href="#services">USER</a>
-        <a href="#portfolio">ADMIN</a>
-        <a href="#contact">DEV</a>
+        <div class="navOptions">
+          <a href="index.html">HOME</a>
+          <a href="explore.html">EXPLORE</a>
+          <a href="#services">USER</a>
+          <a href="#portfolio">ADMIN</a>
+          <a href="#contact">DEV</a>
+        </div>
+
+        <div class="navHamburger">
+          <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                  <i class="bi bi-list"></i>                
+              </button>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo01">                 
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="index.html">HOME</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="explore.html">EXPLORE</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#services">USER</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#portfolio">ADMIN</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#contact">DEV</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
       </nav>
     </header>
   `;
