8f175edd383965344942614cdce99f3764ea2b01
Regis <regis.lopes.eso@gmail.com>
2025-04-23 15:34:57 -0300

    Implement devs page 3

--- js/components/footer.js
+++ js/components/footer.js
@@ -6,6 +6,15 @@ function loadFooter() {
       <div class="footer-text">
         <p>Copyright &copy; by JPR Trainings</p>
       </div>
+
+      <div>
+      Icons sources:
+      <a href="https://loading.io/"> LOADING IO</a>
+      and
+      <a href="https://www.flaticon.com/" title="Flaticon"
+          >FLATICON</a
+        >
+      </div>
       <div class="top-icon">
         <a href="#"><i class="fa-solid fa-arrow-up"></i></a>
       </div>
