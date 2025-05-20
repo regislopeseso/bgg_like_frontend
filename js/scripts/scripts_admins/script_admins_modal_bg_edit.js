d52b26f6404131804418c521c600bcdb48c779da
Regis <regis.lopes.eso@gmail.com>
2025-05-20 14:56:07 -0300

    Implement admins page 4

--- js/scripts/scripts_admins/script_admins_modal_bg_edit.js
+++ js/scripts/scripts_admins/script_admins_modal_bg_edit.js
@@ -4,9 +4,50 @@ function modal_BG_Edit() {
 
   self.LoadReferences = () => {
     self.DOM = $("#bg-add-edit-modal");
+
+    self.Form = $("#bg-add-edit-form");
+
+    self.Inputs = [];
+    self.Inputs[self.Inputs.length] = self.Inputs.Required =
+      self.DOM.find(".required");
+    self.Inputs[self.Inputs.length] = self.Inputs.BgName =
+      self.DOM.find("#new-bg-name");
+    self.Inputs[self.Inputs.length] = self.Inputs.BgDescription = self.DOM.find(
+      "#new-bg-description"
+    );
+    self.Inputs[self.Inputs.length] = self.Inputs.BgMinPlayers = self.DOM.find(
+      "#new-bg-min-players"
+    );
+    self.Inputs[self.Inputs.length] = self.Inputs.BgMaxPlayers = self.DOM.find(
+      "#new-bg-max-players"
+    );
+    self.Inputs[self.Inputs.length] = self.Inputs.BgMinAge =
+      self.DOM.find("#new-bg-min-age");
+
+    self.SelectCategory = $("#new-bg-category-select");
+    self.SelectMechanics = $("#new-bg-mechanics-select");
+
+    self.Buttons = [];
+    self.Buttons[self.Buttons.length] = self.Buttons.Submit =
+      self.DOM.find("#bg-submit-button");
+    self.Buttons[self.Buttons.length] = self.Buttons.Reset = self.DOM.find(
+      "#reset-add-edit-bg-form"
+    );
   };
 
-  self.LoadEvents = () => {};
+  self.LoadEvents = () => {
+    self.Buttons.Submit.on("click", function (e) {
+      e.preventDefault();
+
+      self.SetUpAddBgForm();
+    });
+
+    self.Buttons.Reset.on("click", function (e) {
+      self.forceClearForm();
+    });
+
+    self.CheckForm();
+  };
 
   self.loadCategories = () => {
     // First destroy any existing select2 instance to prevent duplicates
@@ -38,6 +79,10 @@ function modal_BG_Edit() {
           allowClear: true,
           theme: "classic",
           width: "100%",
+          templateSelection: (data) => {
+            if (!data.id) return data.text;
+            return $("<strong>").text(data.text);
+          },
         });
       })
       .catch((err) => {
@@ -75,6 +120,10 @@ function modal_BG_Edit() {
           allowClear: true,
           theme: "classic",
           width: "100%",
+          templateSelection: (data) => {
+            if (!data.id) return data.text;
+            return $("<strong>").text(data.text);
+          },
         });
       })
       .catch((err) => {
@@ -82,23 +131,118 @@ function modal_BG_Edit() {
       });
   };
 
+  self.checkFormFilling = () => {
+    let areFieldsFilled = true;
+
+    const isCategorySelected =
+      self.SelectCategory.val() !== null && self.SelectCategory.val() !== "";
+
+    const areMechanicsSelected =
+      self.SelectMechanics.val() !== null && self.SelectMechanics.val() !== "";
+
+    self.Inputs.Required.each(function () {
+      const value = $(this).val();
+
+      // Skip check if value is null or empty string
+      if (
+        value === null ||
+        (typeof value === "string" && value.trim() === "") ||
+        (Array.isArray(value) && value.length === 0)
+      ) {
+        areFieldsFilled = false;
+      }
+    });
+
+    self.Buttons.Submit.prop(
+      "disabled",
+      !(areFieldsFilled && isCategorySelected && areMechanicsSelected)
+    );
+  };
+
+  self.CheckForm = () => {
+    // React to mechanics selection
+    self.SelectMechanics.on("select2:select", self.checkFormFilling);
+    self.SelectMechanics.on("select2:clear", () => {
+      self.checkFormFilling();
+    });
+    // React to category selection
+    self.SelectCategory.on("select2:select", self.checkFormFilling);
+    self.SelectCategory.on("select2:clear", () => {
+      self.checkFormFilling();
+    });
+    // React to typing in any input
+    self.Inputs.Required.on("input", self.checkFormFilling);
+    // React to clicking on the clear button:
+    self.Buttons.Reset.on("click", () => {
+      self.forceClearForm();
+    });
+  };
+
+  self.forceClearForm = () => {
+    // Block form submission button
+    self.Buttons.Submit.prop("disabled", true);
+
+    //Clear form
+    self.Inputs.forEach((input) => {
+      input.val(null);
+    });
+
+    // Clear catergory and mechanics selection
+    self.SelectCategory.trigger("change");
+    self.SelectMechanics.trigger("change");
+  };
+
   self.SetUpAddBgForm = () => {
-    // Disable submit button to prevent double submissions
-    const submitBtn = $(this).find("button[type='submit']");
+    //Disable submit button to prevent double submissions
+    const submitBtn = self.Buttons.Submit;
     const originalBtnText = submitBtn.text();
     submitBtn.attr("disabled", true).text("Submitting...");
+
+    console.log(self.Form.serialize());
 
     $.ajax({
       type: "POST",
       url: "https://localhost:7081/admins/addboardgame",
-      data: $(this).serialize(),
+      data: self.Form.serialize(),
+
       xhrFields: {
         withCredentials: true,
       },
       success: (resp) => {
         alert(resp.message);
 
-        //forceClearForm();
+        self.forceClearForm();
+      },
+      error: (err) => {
+        alert(err);
+      },
+      complete: () => {
+        // Re-enable button
+        submitBtn.attr("disabled", true).text(originalBtnText);
+      },
+    });
+  };
+
+  self.SetUpEditBgForm = () => {
+    //Disable submit button to prevent double submissions
+    const submitBtn = self.Buttons.Submit;
+    const originalBtnText = submitBtn.text();
+    submitBtn.attr("disabled", true).text("Submitting...");
+
+    console.log(self.Form.serialize());
+
+    $.ajax({
+      type: "PUT",
+      url: "https://localhost:7081/admins/editboardgame",
+      data: self.Form.serialize(),
+
+      xhrFields: {
+        withCredentials: true,
+      },
+      success: (resp) => {
+        alert(resp.message);
+
+        self.forceClearForm();
       },
       error: (err) => {
         alert(err);
