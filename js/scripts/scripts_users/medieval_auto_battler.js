ba2438849498fb88d9132d3dd4133b3a804e5872
Regis <regis.lopes.eso@gmail.com>
2025-08-12 18:02:23 -0300

    Medieval Auto Battler Features 05

--- js/scripts/scripts_users/medieval_auto_battler.js
+++ js/scripts/scripts_users/medieval_auto_battler.js
@@ -2,6 +2,12 @@ function medieval_auto_battler() {
   let self = this;
   self.IsBuilt = false;
 
+  self.DeckSizeLimit = null;
+  self.CurrentDeckSize = 0;
+  self.SelectedCardId = null;
+  self.Deck_CardIds = [];
+  self.Deck_Cards = [];
+
   self.LoadReferences = () => {
     self.DOM = $("#dom-medieval-auto-battler");
 
@@ -9,29 +15,53 @@ function medieval_auto_battler() {
 
     self.MabMenu = self.DOM.find("#mab-menu");
 
-    self.StartNewMabCampaign_FormContainer = self.DOM.find(
-      "#container-mab-start-new-campaign-form"
-    );
     self.StartNewMabCampaign_Form = self.DOM.find(
       "#form-mab-start-new-campaign"
     );
+    self.ActiveDeck_CardsList = self.DOM.find("#ol-mab-player-cardsList");
 
-    self.MabCampaignStats_Container = self.DOM.find(
+    self.Containers = [];
+    self.Containers[self.Containers.length] =
+      self.Containers.StartNewMabCampaign = self.DOM.find(
+        "#container-mab-start-new-campaign-form"
+      );
+    self.Containers.ManageMabPlayerDecks = self.DOM.find(
+      "#container-manage-mab-player-decks"
+    );
+    self.Containers.CreateMabDeck = self.DOM.find("#container-add-mab-deck");
+    self.Containers.MabCampaignStats = self.DOM.find(
       "#container-mab-campaign-stats"
     );
 
     self.Buttons = [];
     self.Buttons[self.Buttons.length] = self.Buttons.OpenStartNewCampaign_Form =
       self.DOM.find("#button-mab-start-new-campaign");
+    self.Buttons[self.Buttons.length] = self.Buttons.StartNewMabCampaign =
+      self.DOM.find("#confirm-mab-start-new-campaign");
     self.Buttons[self.Buttons.length] =
       self.Buttons.CloseStartNewCampaign_Form = self.DOM.find(
         "#button-mab-start-new-campain-close-form"
       );
-    self.Buttons[self.Buttons.length] = self.Buttons.StartNewMabCampaign =
-      self.DOM.find("#confirm-mab-start-new-campaign");
 
-    self.Buttons[self.Buttons.length] = self.Buttons.LoadCampaign =
-      self.DOM.find("#button-mab-load-campaign");
+    self.Buttons[self.Buttons.length] = self.Buttons.ContinueMabCampaign =
+      self.DOM.find("#button-continue-mab-campaign");
+
+    self.Buttons[self.Buttons.length] = self.Buttons.OpenManageMabPlayerDecks =
+      self.DOM.find("#button-open-manage-mab-player-decks");
+    self.Buttons[self.Buttons.length] = self.Buttons.OpenAddMabDeck =
+      self.DOM.find("#button-open-add-mab-deck");
+    self.Buttons[self.Buttons.length] = self.Buttons.CloseAddMabDeck =
+      self.DOM.find("#button-add-mab-deck-close-form");
+
+    self.Buttons[self.Buttons.length] = self.Buttons.CloseManageMabPlayerDecks =
+      self.DOM.find("#button-close-manage-mab-player-decks");
+
+    self.Buttons[self.Buttons.length] = self.Buttons.EditMabPlayerNickname =
+      self.DOM.find("#button-edit-mab-player-nickname");
+    self.Buttons[self.Buttons.length] =
+      self.Buttons.ConfirmEditMabPlayerNickName = self.DOM.find(
+        "#button-confirm-edit-mab-player-nickname"
+      );
 
     self.Buttons[self.Buttons.length] = self.Buttons.OpenCampaignStats =
       self.DOM.find("#button-mab-show-campaign-stats");
@@ -39,7 +69,7 @@ function medieval_auto_battler() {
       self.DOM.find("#button-mab-close-campaign-stats");
 
     self.Inputs = [];
-    self.Inputs[self.Inputs.length] = self.Inputs.MabPlayer_NickName =
+    self.Inputs[self.Inputs.length] = self.Inputs.MabPlayer_Nickname =
       self.DOM.find("#mab-campaign-player-nickname");
     self.Inputs[self.Inputs.length] = self.Inputs.MabCampaignDifficulty_Easy =
       self.DOM.find("#mab-campaign-difficulty-easy");
@@ -47,12 +77,20 @@ function medieval_auto_battler() {
       self.DOM.find("#mab-campaign-difficulty-medium");
     self.Inputs[self.Inputs.length] = self.Inputs.MabCampaignDifficulty_Hard =
       self.DOM.find("#mab-campaign-difficulty-hard");
-    self.Inputs[self.Inputs.length] = self.Inputs.Stats_NewNickName =
+
+    self.Inputs[self.Inputs.length] =
+      self.Inputs.ManageDecks_ActiveDeck_DeckName = self.DOM.find(
+        "#mab-active-deck-name"
+      );
+    self.Inputs[self.Inputs.length] = self.Inputs.AddMabDeck_DeckName =
+      self.DOM.find("#input-add-mab-deck-deckname");
+
+    self.Inputs[self.Inputs.length] = self.Inputs.Stats_NewNickname =
       self.DOM.find("#mab-stats-player-new-nickname");
     self.Inputs[self.Inputs.length] = self.Inputs.Stats_Level =
       self.DOM.find("#mab-stats-level");
     self.Inputs[self.Inputs.length] = self.Inputs.Stats_GoldStash =
-      self.DOM.find("#mab-stats-goldstash");
+      self.DOM.find("#mab-stats-gold-stash");
     self.Inputs[self.Inputs.length] = self.Inputs.Stats_BattlesCount =
       self.DOM.find("#mab-stats-battles-count");
     self.Inputs[self.Inputs.length] = self.Inputs.Stats_BattlesWon =
@@ -63,6 +101,12 @@ function medieval_auto_battler() {
       self.DOM.find("#mab-stats-boosters-opened");
     self.Inputs[self.Inputs.length] = self.Inputs.Stats_BoostersOpened =
       self.DOM.find("#mab-stats-boosters-opened");
+
+    self.Imgs = [];
+    self.Imgs[self.Imgs.length] = self.Imgs.Trophy_AllCardsCollected =
+      self.DOM.find("#allcardscollected-trophy");
+    self.Imgs[self.Imgs.length] = self.Imgs.Trophy_AllNpcsDefeated =
+      self.DOM.find("#allnpcsdefeated-trophy");
 
     self.DifficultyLabels = [];
     self.DifficultyLabels[self.DifficultyLabels.length] =
@@ -87,14 +131,6 @@ function medieval_auto_battler() {
 
       self.StartNewMabCampaign_OpenForm();
     });
-    self.Buttons.CloseStartNewCampaign_Form.on("click", (e) => {
-      e.preventDefault();
-
-      self.StartNewMabCampaign_CloseForm();
-
-      self.MabMainMenu_Open();
-    });
-
     self.Inputs.MabCampaignDifficulty_Easy.on("click", (e) => {
       self.PaintLabel("easy");
     });
@@ -104,17 +140,52 @@ function medieval_auto_battler() {
     self.Inputs.MabCampaignDifficulty_Hard.on("click", (e) => {
       self.PaintLabel("hard");
     });
-
     self.Buttons.StartNewMabCampaign.on("click", (e) => {
       e.preventDefault();
 
       self.StartMabCampaign();
     });
-
-    self.Buttons.LoadCampaign.on("click", (e) => {
+    self.Buttons.CloseStartNewCampaign_Form.on("click", (e) => {
       e.preventDefault();
 
-      self.LoadMabCampaign();
+      self.StartNewMabCampaign_CloseForm();
+
+      self.MabMainMenu_Open();
+    });
+
+    self.Buttons.ContinueMabCampaign.on("click", (e) => {
+      e.preventDefault();
+
+      self.ContinueMabCampaign();
+    });
+
+    self.Buttons.OpenManageMabPlayerDecks.on("click", (e) => {
+      e.preventDefault();
+
+      self.MabMainMenu_Close();
+
+      self.ManageMabPlayerDecks_Open();
+    });
+    self.Buttons.OpenAddMabDeck.on("click", (e) => {
+      e.preventDefault();
+
+      self.ManageMabPlayerDecks_Close();
+
+      self.AddMabDeckForm_Open();
+    });
+    self.Buttons.CloseAddMabDeck.on("click", (e) => {
+      e.preventDefault();
+
+      self.AddMabDeckForm_Close();
+
+      self.ManageMabPlayerDecks_Open();
+    });
+    self.Buttons.CloseManageMabPlayerDecks.on("click", (e) => {
+      e.preventDefault();
+
+      self.ManageMabPlayerDecks_Close();
+
+      self.MabMainMenu_Open();
     });
 
     self.Buttons.OpenCampaignStats.on("click", (e) => {
@@ -123,6 +194,27 @@ function medieval_auto_battler() {
       self.MabMainMenu_Close();
 
       self.MabCampaignStats_Open();
+    });
+    self.Buttons.EditMabPlayerNickname.on("click", (e) => {
+      e.preventDefault();
+      self.Inputs.Stats_NewNickname.prop("readonly", false)
+        .removeClass("current-data")
+        .addClass("new-data")
+        .trigger("select");
+
+      self.Buttons.EditMabPlayerNickname.prop("disabled", true).addClass(
+        "current-data"
+      );
+
+      self.Buttons.ConfirmEditMabPlayerNickName.prop(
+        "disabled",
+        false
+      ).removeClass("current-data");
+    });
+    self.Buttons.ConfirmEditMabPlayerNickName.on("click", (e) => {
+      e.preventDefault();
+
+      self.EditMabPlayerNickName();
     });
     self.Buttons.CloseCampaignStats.on("click", (e) => {
       e.preventDefault();
@@ -170,6 +262,10 @@ function medieval_auto_battler() {
   }
 
   self.ToggleVisibility = (div) => {
+    self.Containers.forEach((container) => {
+      container.addClass("hide-div");
+    });
+
     div.hasClass("show-div")
       ? div.removeClass("show-div").addClass("hide-div")
       : div.removeClass("hide-div").addClass("show-div");
@@ -183,13 +279,8 @@ function medieval_auto_battler() {
   };
 
   self.StartNewMabCampaign_OpenForm = () => {
-    self.ToggleVisibility(self.StartNewMabCampaign_FormContainer);
-    self.Inputs.MabPlayer_NickName.trigger("focus");
-  };
-  self.StartNewMabCampaign_CloseForm = () => {
-    self.Inputs.MabPlayer_NickName.val("");
-
-    self.ToggleVisibility(self.StartNewMabCampaign_FormContainer);
+    self.ToggleVisibility(self.Containers.StartNewMabCampaign);
+    self.Inputs.MabPlayer_Nickname.trigger("focus");
   };
   self.PaintLabel = (difficulty) => {
     self.DifficultyLabels.Easy.removeClass("paint-green");
@@ -233,16 +324,229 @@ function medieval_auto_battler() {
       },
     });
   };
-
   self.TriggerMabBattle = () => {};
+  self.StartNewMabCampaign_CloseForm = () => {
+    self.Inputs.MabPlayer_Nickname.val("");
 
-  self.LoadMabCampaign = () => {};
+    self.ToggleVisibility(self.Containers.StartNewMabCampaign);
+  };
+
+  self.ManageMabPlayerDecks_Open = () => {
+    self.ToggleVisibility(self.Containers.ManageMabPlayerDecks);
+
+    self.FetchMabPlayerActiveDeck();
+  };
+  self.FetchMabPlayerActiveDeck = () => {
+    $.ajax({
+      type: "GET",
+      url: "https://localhost:7081/users/showactivemabdeckdetails",
+      xhrFields: { withCredentials: true },
+      success: function (response) {
+        if (response.content == null) {
+          sweetAlertError("Error", response.message);
+          return;
+        }
+
+        let activeMabDeck = response.content;
+        let activeMabDeckId = response.content.activeMabDeckId;
+        let activeMabDeckName = activeMabDeck.activeMabDeckName;
+        let activeMabCards = activeMabDeck.activeMabPlayerCards;
+
+        self.Inputs.ManageDecks_ActiveDeck_DeckName.html();
+        self.ActiveDeck_CardsList.empty();
+
+        self.Inputs.ManageDecks_ActiveDeck_DeckName.html(
+          `<h4 class="m-0 p-0">${activeMabDeckName}</h4>`
+        );
+
+        activeMabCards.forEach((card, Index) => {
+          let cardId = card.mabCardId;
+          let cardName = card.mabCardName;
+          let cardLvl = card.mabCardLevel;
+          let cardType = card.mabCardType;
+          let cardPower = card.mabCardPower;
+          let cardUpperHand = card.mabCardUpperHand;
+
+          let listItem = `
+            <li id="li-mab-${Index}" data-card-id="${cardId}">
+              <div class="d-flex flex-row align-items-center gap-2">
+                <button
+                  class="button-modal-mab-npcs-removecard btn btn-outline-danger p-0 m-0"
+                  type="button"
+                  data-card-id="${cardId}"
+                  data-index="${self.Index}"
+                >
+                  <i class="fa-solid fa-xmark p-1 m-0"></i>
+                </button>
+
+                <strong class="mab-card-name p-0 m-0">${cardName}</strong>
+
+                <img
+                  src="/images/icons/io_arrow_right.svg"
+                  class="bi bi-arrow p-0 m-0"
+                />
+
+                <div class="mab-card-data">
+
+                  <span>L</span>evel: <strong>${cardLvl}</strong>,
+                  <span>T</span>ype: <strong>${cardType}</strong>,
+                  <span>P</span>ower: <strong>${cardPower}</strong>,
+                  <span>U</span>pper <span>H</span>and:
+                  <strong>${cardUpperHand}</strong>
+                </div>
+              </div>
+            </li>
+          `;
+
+          self.ActiveDeck_CardsList.append(listItem);
+        });
+      },
+      error: function (xhr, status, error) {
+        sweetAlertError(
+          "Failed to fetch active deck details. Try again later."
+        );
+      },
+      complete: function () {},
+    });
+  };
+  self.AddMabDeckForm_Open = () => {
+    self.ToggleVisibility(self.Containers.CreateMabDeck);
+
+    self.Inputs.AddMabDeck_DeckName.trigger("focus");
+  };
+  self.CreateMabDeck = () => {
+    $.ajax({
+      type: "POST",
+      url: "https://localhost:7081/users/addmabdeck",
+      data: self.Form.serialize(),
+      xhrFields: {
+        withCredentials: true,
+      },
+      contentType: "application/json",
+      data: JSON.stringify({
+        MabDeckName: self.Inputs.MabNpcName.val(),
+        MabCardIds: self.Deck_CardIds,
+      }),
+      success: (resp) => {
+        if (!resp.content) {
+          sweetAlertError(resp.message);
+
+          return;
+        }
+        sweetAlertSuccess(resp.message);
+      },
+      error: (err) => {
+        sweetAlertError(err);
+      },
+      complete: () => {},
+    });
+  };
+  self.AddMabDeckForm_Close = () => {
+    self.ToggleVisibility(self.Containers.CreateMabDeck);
+  };
+  self.ManageMabPlayerDecks_Close = () => {
+    self.ToggleVisibility(self.Containers.ManageMabPlayerDecks);
+  };
+
+  self.ContinueMabCampaign = () => {};
 
   self.MabCampaignStats_Open = () => {
-    self.ToggleVisibility(self.MabCampaignStats_Container);
+    self.ToggleVisibility(self.Containers.MabCampaignStats);
+
+    $.ajax({
+      type: "GET",
+      url: `https://localhost:7081/users/showmabcampaignstatistics`,
+      xhrFields: { withCredentials: true },
+      success: function (response) {
+        if (!response.content) {
+          sweetAlertError(response.message_text);
+          return;
+        }
+
+        let mabCampaignDB = response.content;
+        self.Inputs.Stats_NewNickname.val(mabCampaignDB.mabPlayerNickName).css(
+          "color",
+          "var(--main-color)"
+        );
+        self.Inputs.Stats_Level.html(
+          `<span>${mabCampaignDB.playerLevel}</span>`
+        );
+        self.Inputs.Stats_GoldStash.html(
+          `<span>${mabCampaignDB.goldstash}</span>`
+        );
+        self.Inputs.Stats_BattlesCount.html(
+          `<span>${mabCampaignDB.countMatches}</span>`
+        );
+        self.Inputs.Stats_BattlesWon.html(
+          `<span>${mabCampaignDB.countVictories}</span>`
+        );
+        self.Inputs.Stats_BattlesLost.html(
+          `<span>${mabCampaignDB.countDefeats}</span>`
+        );
+        self.Inputs.Stats_BoostersOpened.html(
+          `<span>${mabCampaignDB.countBoosters}</span>`
+        );
+
+        console.log(
+          "mabCampaignDB.allCardsCollectedTrophy: ",
+          mabCampaignDB.allCardsCollectedTrophy
+        );
+        if (mabCampaignDB.allCardsCollectedTrophy == true) {
+          self.Imgs.Trophy_AllCardsCollected.attr(
+            "src",
+            "/images/icons/trophy_allcardscollected_achieved.svg"
+          );
+        }
+
+        console.log(
+          "mabCampaignDB.allNpcsDefeatedTrophy: ",
+          mabCampaignDB.allNpcsDefeatedTrophy
+        );
+        if (mabCampaignDB.allNpcsDefeatedTrophy == true) {
+          self.Imgs.Trophy_AllNpcsDefeated.attr(
+            "src",
+            "/images/icons/trophy_allnpcsdefeated_achieved.svg"
+          );
+        }
+      },
+      error: function (xhr, status, error) {
+        sweetAlertError("Could not load life counter");
+      },
+    });
+  };
+  self.EditMabPlayerNickName = () => {
+    let newMabPlayerNickname = self.Inputs.Stats_NewNickname.val().trim();
+
+    if (!newMabPlayerNickname || newMabPlayerNickname.length < 1) {
+      sweetAlertError("Please fill the Nickname field!");
+
+      return;
+    }
+
+    $.ajax({
+      type: "PUT",
+      url: "https://localhost:7081/users/editmabplayernickname",
+      data: JSON.stringify({
+        NewMabPlayerNickname: newMabPlayerNickname,
+      }),
+      contentType: "application/json",
+      xhrFields: {
+        withCredentials: true,
+      },
+      success: (resp) => {
+        if (resp.content === null) {
+          sweetAlertError(resp.message);
+          return;
+        }
+      },
+      error: (err) => {
+        sweetAlertError(err);
+      },
+      complete: () => {},
+    });
   };
   self.MabCampaignStats_Close = () => {
-    self.ToggleVisibility(self.MabCampaignStats_Container);
+    self.ToggleVisibility(self.Containers.MabCampaignStats);
   };
 
   self.Build = () => {
